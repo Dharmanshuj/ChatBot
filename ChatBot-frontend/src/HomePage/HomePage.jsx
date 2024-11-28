7a94c77c2042204912f0381396e39cc49d4b1114
Dharmanshuj <dharmanshuj.cs.22@nitj.ac.in>
2024-11-28 15:00:38 +0530

    Commit

--- ChatBot-frontend/src/HomePage/HomePage.jsx
+++ ChatBot-frontend/src/HomePage/HomePage.jsx
@@ -44,7 +44,7 @@ const HomePage = () => {
 
   const generateResponse = async (message) => {
     try {
-      const response = await fetch('http://localhost:5000/index', {
+      const response = await fetch('chat-bot-backend-red.vercel.app/index', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message }),
